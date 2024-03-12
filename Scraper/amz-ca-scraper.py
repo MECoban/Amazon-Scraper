456275d102888dadb66ff90bbf9640808540ac78
Mehmet Emin <cobanmehmet3525@gmail.comm>
2024-03-12 03:11:10 +0100

    cleaned working code

--- Scraper/amz-ca-scraper.py
+++ Scraper/amz-ca-scraper.py
@@ -14,8 +14,8 @@ urls = []
 prices = []
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',  # Replace with a user agent string
-    'Accept-Language': 'en-US,en;q=0.9',
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",  # Replace with a user agent string
+    "Accept-Language": "en-US,en;q=0.9",
 }
 
 for p_num in range(1, num_pages + 1):
@@ -23,7 +23,9 @@ for p_num in range(1, num_pages + 1):
     print(f"Processing {page_url}")
     try:
         r = s.get(page_url, headers=headers)
-        r.html.render(sleep=random.uniform(5, 8))  # Randomized sleep to mimic human behavior
+        r.html.render(
+            sleep=random.uniform(5, 8)
+        )  # Randomized sleep to mimic human behavior
         items = r.html.find("div[data-asin]")
 
         for item in items:
@@ -31,30 +33,30 @@ for p_num in range(1, num_pages + 1):
             if asin:
                 asins.append(asin)
                 urls.append(f"https://www.amazon.ca/dp/{asin}")
-                
-                # Assuming these are the correct selectors
+
                 title_selector = "span.a-size-medium.a-color-base.a-text-normal"
                 price_selector = "span.a-price > span.a-offscreen"
-                
+
                 title_element = item.find(title_selector, first=True)
-                titles.append(title_element.text if title_element else "Title not found")
+                titles.append(
+                    title_element.text if title_element else "Title not found"
+                )
 
                 price_element = item.find(price_selector, first=True)
-                prices.append(price_element.text if price_element else "Price not found")
+                prices.append(
+                    price_element.text if price_element else "Price not found"
+                )
 
     except Exception as e:
         print(f"An error occurred on page {p_num}: {e}")
 
     time.sleep(random.uniform(3, 9))  # Sleep after each page request
 
-# Verify data consistency
-if len(asins) == len(titles) == len(urls) == len(prices):
-    products_df = pd.DataFrame({
-        "ASIN": asins,
-        "Title": titles,
-        "URL": urls,
-        "Price": prices
-    })
+
+if len(asins) == len(titles) == len(urls) == len(prices):  # Verify data consistency
+    products_df = pd.DataFrame(
+        {"ASIN": asins, "Title": titles, "URL": urls, "Price": prices}
+    )
     try:
         products_df.to_csv("data/home-kitchen_feature_ca.csv", index=False)
         print("CSV file has been saved.")
