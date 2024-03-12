8aead0ff545cacec4678f34c5f37273e2c6e1e21
MECoban <156511598+MECoban@users.noreply.github.com>
2024-03-12 03:21:58 +0100

    Update amz-ca-scraper.py

--- Scraper/amz-ca-scraper.py
+++ Scraper/amz-ca-scraper.py
@@ -13,6 +13,7 @@ titles = []
 urls = []
 prices = []
 
+#for User-Agent check this website: https://www.whatsmyua.info
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
     "Accept-Language": "en-US,en;q=0.9",
